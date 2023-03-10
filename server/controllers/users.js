6152de08bc0da30538b4d7647d5b7957be25ad8d
abimanyu-abi <abisquare0215@gmail.com>
2023-03-10 15:56:03 +0530

    socail media app completed

---
+++ server/controllers/users.js
@@ -0,0 +1,65 @@
+import User from "../models/User.js";
+
+/* READ */
+export const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+/* UPDATE */
+export const addRemoveFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const use = await User.findById(id);
+    const frien = await User.findById(friendId);
+
+    if (use.friends.includes(friendId)) {
+      use.friends = use.friends.filter((id) => id !== friendId);
+      frien.friends = frien.friends.filter((id) => id !== id);
+    } else {
+      use.friends.push(friendId);
+      frien.friends.push(id);
+    }
+    
+    const user = await User.findByIdAndUpdate(use._id,{friends:use.friends})
+    const friend = await User.findByIdAndUpdate(frien._id,{friends:frien.friends})
+    
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
